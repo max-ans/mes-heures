dc6c2801e15c99116f64426f040000ed817628cd
max-ans <maxenceroyer@hotmail.com>
2021-03-31 10:30:12 +0200

    AltoDispatcher

--- public/index.php
+++ public/index.php
@@ -17,5 +17,18 @@ if (array_key_exists("BASE_URI" , $_SERVER)) {
 require_once '../app/Utils/Routes.php';
 
 $match = $router->match();
+dump($match);
+$dispatcher = new Dispatcher(
+    $match,
+    'App\Controllers\ErrorController::error'
+);
 
-dd($match);
+$dispatcher->setControllersArguments($router);
+
+if (!empty($match['params'])) {
+   $dispatcher->setControllersArguments($router, $match['params']);
+}
+
+$dispatcher->setControllersNamespace('App\Controllers');
+dd($dispatcher);
+$dispatcher->dispatch();
